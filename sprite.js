bbae310d9416ea92216e782556a275303821a887
Luidgi Sarto Lacerda <lslacerda@stefanini.com>
2017-04-18 17:11:30 -0300

    Inicio Moon Lander

---
+++ sprite.js
@@ -0,0 +1,17 @@
+function Sprite(){
+    this.g = 0;
+    this.x = 100;
+    this.y = 100;
+    this.vx = 0;
+    this.vy = 0;
+    this.ax = 0;
+    this.ay = 0;
+    this.width = 30;
+    this.height = 30;
+    this.angle = 0;
+    this.vang = 0;
+    this.color = "green";
+}
+
+Sprite.prototype.desenhar = function(ctx){
+}
