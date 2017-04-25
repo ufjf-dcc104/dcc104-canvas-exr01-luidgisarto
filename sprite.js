7a0915d2d87dd81aed2dbc1d6ece3d6da0295866
Luidgi Sarto Lacerda <lslacerda@stefanini.com>
2017-04-25 10:21:28 -0300

    Adicionado dinâmica de movimento

--- sprite.js
+++ sprite.js
@@ -1,30 +1,37 @@
-function Sprite(){
-    this.g = 0;
-    this.x = 100;
-    this.y = 100;
-    this.vx = 0;
-    this.vy = 0;
-    this.ax = 0;
-    this.ay = 0;
-    this.width = 30;
-    this.height = 30;
-    this.angle = 0;
-    this.vang = 0;
-    this.color = "blue";
-    this.energia = 100;
+function Sprite() {
+  this.x = 10;
+  this.y = 430;
+  this.vx = 0;
+  this.vy = 0;
+  this.ax = 0;
+  this.ay = 0;
+  this.width = 30;
+  this.height = 30;
+  this.color = "blue";
+  this.energia = 500;
+  this.cooldown = 0;
 }
 
 
 Sprite.prototype.desenhar = function (ctx) {
+  //Desenha nave
   ctx.fillStyle = this.color;
-  ctx.fillRect(this.x, this.y, 15, 15);
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(this.x, this.y, 15, 15);
+  ctx.fillRect(this.x, this.y, 50, 60);
 };
 
 Sprite.prototype.mover = function (dt) {
-  this.vx = this.vx + this.ax*dt;
-  this.vy = this.vy + (this.ay+60)*dt;
-  this.x = this.x + this.vx*dt;
-  this.y = this.y + this.vy*dt;
+  this.vx = this.vx + this.ax * dt;
+  this.x = this.x + this.vx * dt;
+  this.vy = this.vy + this.ay * dt;
+  this.y = this.y + this.vy * dt;
+
+  this.consumirEnergia();
 };
+
+Sprite.prototype.consumirEnergia = function (){
+  if(this.ax > 0 || this.ay > 0){
+    this.energia = this.energia - Math.floor(Math.random() *1.7);
+  }
+}
+
+
